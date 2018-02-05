5fa3adf6512c40f9805fa27869ad1292575a2d49
midasPrimaris <theelderone02@gmail.com>
2018-02-05 16:29:22 -0500

    Smart Chase Enemy Charges at Player

--- Assets/Characters/Enemies/AI Scripts/SmarterChaseEnemy.cs
+++ Assets/Characters/Enemies/AI Scripts/SmarterChaseEnemy.cs
@@ -3,14 +3,17 @@
 public class SmarterChaseEnemy : MonoBehaviour
 {
     [SerializeField] float backupRadius = 3;
-    [SerializeField] float speed = 10;
+    [SerializeField] float speed = 3;
+    [SerializeField] float chargeSpeed = 6;
     [SerializeField] Transform wallCheck;
 
     bool canChase = false;
     Transform player;
+    BaseEnemy baseEnemy;
 
     void Awake()
     {
+        baseEnemy = GetComponent<BaseEnemy>();
         player = FindObjectOfType<Player>().gameObject.transform;
     }
 
@@ -20,12 +23,20 @@ public class SmarterChaseEnemy : MonoBehaviour
 
         if (canChase)
         {
+            if(distanceToPlayer <= backupRadius && baseEnemy.hasBeenShot)
+            {
+                transform.position = Vector3.MoveTowards(
+                    transform.position,
+                    new Vector3(player.position.x, transform.position.y, transform.position.z),
+                    chargeSpeed * Time.deltaTime
+                );
+            }
             if(distanceToPlayer <= backupRadius)
             {
                 transform.position = Vector3.MoveTowards(
                     transform.position,
                     new Vector3(wallCheck.position.x, transform.position.y, transform.position.z),
-                    player.GetComponent<Player>().speed - 1
+                    player.GetComponent<Player>().speed * Time.deltaTime
                 );
             }
 
@@ -34,7 +45,7 @@ public class SmarterChaseEnemy : MonoBehaviour
                 transform.position = Vector3.MoveTowards(
                     transform.position,
                     new Vector3(player.position.x, transform.position.y, transform.position.z),
-                    speed
+                    speed * Time.deltaTime
                 );
             }
         }

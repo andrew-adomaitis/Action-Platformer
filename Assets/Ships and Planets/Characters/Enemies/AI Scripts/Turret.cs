4cb45b5b7843f5d4e7e57ffad18f7cb367b4249b
midasPrimaris <theelderone02@gmail.com>
2018-02-23 16:57:30 -0500

    Added the Sprites for Mercenary Equipment

---
+++ Assets/Ships and Planets/Characters/Enemies/AI Scripts/Turret.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    [SerializeField] Transform turretShooter;
+
+    BaseEnemy baseEnemy;
+    Transform player;
+    
+    void Awake()
+    {
+        player = FindObjectOfType<Player>().transform;
+        baseEnemy = GetComponent<BaseEnemy>();
+    }
+
+    void Update()
+    {
+        Vector3 diff = player.position - turretShooter.position;
+        diff.Normalize();
+
+        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        turretShooter.rotation = Quaternion.Euler(0f, 0f, rot_z - 180);
+    }
+}
